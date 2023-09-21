ae3e9e8ba77df7b899359943c33044817574e822
chloejepson <jepsoc@rpi.edu>
2023-09-20 22:00:25 -0400

    working connection and endpoint

--- backend/routes/record.js
+++ backend/routes/record.js
@@ -1,4 +1,43 @@
 const express = require("express");
+const app = express();
+const bodyParser= require("body-parser");
+app.use(bodyParser.json());
+var jsonParser = bodyParser.json();
+
+const {MongoClient, ServerApiVersion}= require('mongodb');
+const { default: mongoose } = require('mongoose');
+const uri= "mongodb+srv://troyboy:websci@cluster0.6dafd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var ObjectId = require('mongodb').ObjectID;
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}));
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/db', async (req, res) => {
+  try {
+      await client.connect();
+      const collection = client.db("PracticeDB").collection("Users");
+      const cursor = await collection.find().toArray();
+      res.json(cursor);
+  } catch(e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+})
  
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
@@ -9,23 +48,29 @@ const recordRoutes = express.Router();
 const dbo = require("../db/conn.js");
  
 // This helps convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
+//const ObjectId = require("mongodb").ObjectId;
  
  
 // This section will help you get a list of all the records.
-recordRoutes.route("/user").get(function (req, res) {
+recordRoutes.route("/user").get(async function (req, res) {
  let db_connect = dbo.getDatabase("PracticeDB");
  if(db_connect){
   console.log("getDatabase working");
  }
  console.log(db_connect.collection("Users"))
- db_connect
-   .collection("Users")
-   .findOne({}, function (err, result) {
-     if (err) throw err;
-     res.json(result);
-     console.log(result)
-   });
+//  db_connect
+//    .collection("Users")
+//    .findOne({}, function (err, result) {
+//      if (err) throw err;
+//      res.json(result);
+//      console.log(result)
+//    });
+try {
+  const answer = await db_connect.collection("Users").find({ firstName: "Chloe" }).toArray();
+  console.log(answer);
+} catch (error) {
+  console.error(error);
+}
     
 });
 
@@ -34,13 +79,9 @@ recordRoutes.route("/user").get(function (req, res) {
 // This section will help you get a single record by id
 recordRoutes.route("/user/:id").get(function (req, res) {
  let db_connect = dbo.getDb();
- let myquery = { _id: ObjectId(req.params.id) };
- db_connect
-   .collection("Users")
-   .findOne(myquery, function (err, result) {
-     if (err) throw err;
-     res.json(result);
-   });
+ //let myquery = { _id: ObjectId(req.params.id) };
+ let answer= db_connect.collection("Users").find({firstName: "chloe"}).toArray();
+ console.log(answer);
 });
  
 // This section will help you create a new record.
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
+const {MongoClient, ServerApiVersion}= require('mongodb');
+
+const bodyParser= require("body-parser");
+const path= require('path');
+const uri= "mongodb+srv://chloej1699:SnowBird11@cluster0.8ihubjl.mongodb.net/"
+app.use(bodyParser.json());
 
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
@@ -11,10 +17,27 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(require("./routes/record"));
+//next: clean up record.js and conn.js -- not rlly being used
 
 // Get MongoDB driver connection
 const dbo = require("./db/conn.js");
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}));
  
 app.listen(port, () => {
   // Perform a database connection when server starts
@@ -24,3 +47,23 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/db', async (req, res) => {
+  try {
+      await client.connect();
+      const collection = client.db("PracticeDB").collection("Users");
+      if(collection){
+        console.log("found collection")
+      }
+      const cursor = await collection.find().toArray();
+      console.log(cursor);
+      console.log(res.json(cursor));
+      res.json(cursor);
+  } catch(e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+})
